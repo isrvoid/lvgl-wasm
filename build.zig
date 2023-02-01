d76618043d9536b8b219f5a69fa3e2687f064f1f
Johannes Teichrieb <isrvoid@gmail.com>
2023-02-01 21:23:23 +0100

    Generate bitmaps from PNG images during build

--- build.zig
+++ build.zig
@@ -1,24 +1,12 @@
 const std = @import("std");
 const mem = std.mem;
-
-fn getLvglSources(allocator: std.mem.Allocator) ![]const []const u8 {
-    var res = allocator.alloc([]const u8, 200) catch unreachable;
-    const dir = try std.fs.cwd().openIterableDir("lvgl/src", .{});
-    var walker = try dir.walk(allocator);
-    defer walker.deinit();
-    var src_i: usize = 0;
-    while (try walker.next()) |e| {
-        if (mem.endsWith(u8, e.basename, ".c")) {
-            const sep_path = [2][]const u8{ "lvgl/src/", e.path };
-            res[src_i] = std.mem.concat(allocator, u8, &sep_path) catch unreachable;
-            src_i += 1;
-        }
-    }
-    return res[0..src_i];
-}
+const heap = std.heap;
+const fs = std.fs;
+const zpng = @import("zpng/zpng.zig");
 
 pub fn build(b: *std.build.Builder) !void {
     const wasm_target = std.zig.CrossTarget.parse(.{ .arch_os_abi = "wasm32-freestanding" }) catch unreachable;
+    try maybeGenerateImageBitmaps();
 
     const gui = b.addStaticLibrary(.{
         .name = "lvgl_gui",
@@ -26,13 +14,15 @@ pub fn build(b: *std.build.Builder) !void {
         .target = wasm_target,
         .optimize = .ReleaseFast,
     });
-    gui.addCSourceFile("src/libc/lvgl_libc.c", &[0][]u8{});
-    gui.addCSourceFile("src/lvgl_init.c", &[0][]u8{});
-    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    gui.addCSourceFile("src/libc/lvgl_libc.c", &.{});
+    gui.addCSourceFile("src/lvgl_init.c", &.{});
+    var arena = heap.ArenaAllocator.init(heap.page_allocator);
     const lvgl_sources = try getLvglSources(arena.allocator());
-    gui.addCSourceFiles(lvgl_sources, &[0][]u8{});
+    gui.addCSourceFiles(lvgl_sources, &.{});
     gui.addIncludePath("src/libc/include");
     gui.addIncludePath("lvgl/src");
+    gui.addPackage(.{ .name = "init_image", .source = .{ .path = "src/init_image.zig" } });
+    gui.addObjectFile("images/images.zig");
 
     const bind = b.addSharedLibrary(.{
         .name = "webfb",
@@ -46,4 +36,85 @@ pub fn build(b: *std.build.Builder) !void {
     const wasm_page_size = 1 << 16;
     bind.global_base = 8 * wasm_page_size;
     bind.install();
+}
+
+fn maybeGenerateImageBitmaps() !void {
+    var arena = heap.ArenaAllocator.init(heap.page_allocator);
+    defer arena.deinit();
+    const png_names = try getPngNames(arena.allocator());
+    for (png_names) |name|
+        try maybeGenerateBitmap(name);
+}
+
+fn getPngNames(allocator: mem.Allocator) ![]const []const u8 {
+    return getFilesWithEnding(allocator, ".png", "images");
+}
+
+fn maybeGenerateBitmap(png_path: []const u8) !void {
+    var path_buf: [fs.MAX_PATH_BYTES]u8 = undefined;
+    const bmp_path = replaceEnding(png_path, ".raw", &path_buf);
+    const cwd = fs.cwd();
+    const png_file = try cwd.openFile(png_path, .{});
+    defer png_file.close();
+    const png_mod_time = (try png_file.metadata()).modified();
+    const should_generate = res: {
+        const bmp_ = cwd.openFile(bmp_path, .{}) catch null;
+        if (bmp_ == null) break :res true;
+        const bmp = bmp_.?;
+        defer bmp.close();
+        break :res (try bmp.metadata()).modified() < png_mod_time;
+    };
+    if (!should_generate) return;
+    var arena = heap.ArenaAllocator.init(heap.page_allocator);
+    defer arena.deinit();
+    const bitmap = try decodePng(arena.allocator(), png_file);
+    const bmp_file = try cwd.createFile(bmp_path, .{});
+    defer bmp_file.close();
+    try writeBitmap(bitmap, bmp_file);
+}
+
+fn replaceEnding(src: []const u8, ending: []const u8, buf: []u8) []const u8 {
+    const res = buf[0..src.len];
+    mem.copy(u8, res, src);
+    mem.copy(u8, res[res.len - ending.len ..], ending);
+    return res;
+}
+
+fn decodePng(allocator: mem.Allocator, file: fs.File) !zpng.Image {
+    var buf = std.io.bufferedReader(file.reader());
+    return try zpng.Image.read(allocator, buf.reader());
+}
+
+fn writeBitmap(bmp: zpng.Image, file: fs.File) !void {
+    std.debug.assert(bmp.pixels.len % bmp.width == 0);
+    var buf = std.io.bufferedWriter(file.writer());
+    var wr = buf.writer();
+    var pix8: [4]u8 = undefined;
+    for (bmp.pixels) |pix| {
+        pix8[0] = @truncate(u8, pix[0] >> 8);
+        pix8[1] = @truncate(u8, pix[1] >> 8);
+        pix8[2] = @truncate(u8, pix[2] >> 8);
+        pix8[3] = @truncate(u8, pix[3] >> 8);
+        try wr.writeAll(&pix8);
+    }
+    try wr.writeAll(mem.asBytes(&bmp.width));
+    try buf.flush();
+}
+
+fn getLvglSources(allocator: mem.Allocator) ![]const []const u8 {
+    return getFilesWithEnding(allocator, ".c", "lvgl/src");
+}
+
+fn getFilesWithEnding(allocator: mem.Allocator, ending: []const u8, sub_path: []const u8) ![]const []const u8 {
+    var res = std.ArrayList([]const u8).initCapacity(allocator, 0x100) catch unreachable;
+    var dir = try fs.cwd().openIterableDir(sub_path, .{});
+    defer dir.close();
+    var walker = try dir.walk(allocator);
+    defer walker.deinit();
+    while (try walker.next()) |e| {
+        if (!mem.endsWith(u8, e.basename, ending)) continue;
+        const path_parts = [3][]const u8{ sub_path, "/", e.path };
+        (res.addOne() catch unreachable).* = mem.concat(allocator, u8, &path_parts) catch unreachable;
+    }
+    return res.toOwnedSlice() catch unreachable;
 }
