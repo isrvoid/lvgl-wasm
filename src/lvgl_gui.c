e3a12196f2f289206f428c4ad6600c5fdb23ee7b
Johannes Teichrieb <isrvoid@gmail.com>
2023-03-14 23:45:20 +0100

    Move widgets demo to examples repo

--- src/lvgl_gui.c
+++
@@ -1,9 +0,0 @@
-#include <lvgl.h>
-
-#undef LV_USE_DEMO_WIDGETS
-#define LV_USE_DEMO_WIDGETS 1
-#include "../demos/widgets/lv_demo_widgets.c"
-
-void create_lvgl_gui(void) {
-    lv_demo_widgets();
-}
